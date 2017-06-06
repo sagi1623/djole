1ac459ded493fea210f3855b8833b4088097a50f
sagi1623 <sagi1623@gmail.com>
2017-06-06 10:05:01 +0200

    Controllers Region,Country,Place,Comment added

---
+++ BookingAppInitial-master/BookingApp/BookingApp/Controllers/CommentController.cs
@@ -0,0 +1,125 @@
+﻿using BookingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace BookingApp.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api")]
+    public class CommentController : ApiController
+    {
+        private BAContext db = new BAContext();
+
+        [HttpGet]
+        [Route("Comments")]
+        public IQueryable<Comment> GetComments()
+        {
+            return db.Comments;
+        }
+
+        [HttpGet]
+        [Route("Comments/{accId}/{appId}")]
+        public IHttpActionResult GetComment(int accId, int appId)
+        {
+            Comment comment = db.Comments.Find(accId,appId);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(comment);
+        }
+
+        [HttpPut]
+        [Route("Comments/{accId}/{appId}")]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutComment(int accId, int appId, Comment comment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if ((accId != comment.AccommodationId )|| (appId != comment.AppUserId))
+            {
+                return BadRequest();
+            }
+
+            db.Entry(comment).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CommentExists(accId,appId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [HttpPost]
+        [Route("Comments")]
+        [ResponseType(typeof(Comment))]
+        public IHttpActionResult PostComment(Comment comment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Comments.Add(comment);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { accid = comment.AccommodationId , appId = comment.AppUserId }, comment);
+        }
+
+        [HttpDelete]
+        [Route("Comments/{accId}/{appId}")]
+        [ResponseType(typeof(Comment))]
+        public IHttpActionResult DeleteComment(int accId, int appId)
+        {
+            Comment comment = db.Comments.Find(accId,appId);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            db.Comments.Remove(comment);
+            db.SaveChanges();
+
+            return Ok(comment);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CommentExists(int accId, int appId)
+        {
+            return db.Comments.Count(e => (e.AccommodationId == accId) &&(e.AppUserId == appId)) > 0;
+        }
+
+
+    }
+}
---
+++ BookingAppInitial-master/BookingApp/BookingApp/Controllers/PlaceController.cs
@@ -0,0 +1,124 @@
+﻿using BookingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace BookingApp.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api")]
+    public class PlaceController : ApiController
+    {
+        private BAContext db = new BAContext();
+
+        [HttpGet]
+        [Route("Places")]
+        public IQueryable<Place> GetPlaces()
+        {
+            return db.Places;
+        }
+
+        [HttpGet]
+        [Route("Places/{id}")]
+        public IHttpActionResult GetPlace(int id)
+        {
+            Place place = db.Places.Find(id);
+            if (place == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(place);
+        }
+
+        [HttpPut]
+        [Route("Places/{id}")]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPlace(int id, Place place)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != place.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(place).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlaceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [HttpPost]
+        [Route("Places")]
+        [ResponseType(typeof(Place))]
+        public IHttpActionResult PostPlace(Place place)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Places.Add(place);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = place.Id }, place);
+        }
+
+        [HttpDelete]
+        [Route("Places/{id}")]
+        [ResponseType(typeof(Place))]
+        public IHttpActionResult DeletePlace(int id)
+        {
+            Place place = db.Places.Find(id);
+            if (place == null)
+            {
+                return NotFound();
+            }
+
+            db.Places.Remove(place);
+            db.SaveChanges();
+
+            return Ok(place);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PlaceExists(int id)
+        {
+            return db.Places.Count(e => e.Id == id) > 0;
+        }
+
+    }
+}
