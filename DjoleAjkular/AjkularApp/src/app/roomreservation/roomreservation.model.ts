d5a2d4f215326137685590cc0b25d3a421e5ea02
borka94 <borka.pzs@gmail.com>
2017-06-08 13:30:42 +0200

    References for initial model for Accommodation, AccommodationType, Room and RoomReservation added

--- DjoleAjkular/AjkularApp/src/app/roomreservation/roomreservation.model.ts
+++ DjoleAjkular/AjkularApp/src/app/roomreservation/roomreservation.model.ts
@@ -1,6 +1,8 @@
+import { Room } from "app/room/room.model";
+
 export class RoomReservation {
 
-    constructor(public StartDate:Date, public EndDate:Date)
+    constructor(public StartDate:Date, public EndDate:Date, public Room:Room)
     {
     }
 
