de965ea0b2b4fa0cece72e022d0727a96b8e8e65
sagi1623 <sagi1623@gmail.com>
2017-06-08 13:32:20 +0200

    Model references added for comment and place

--- DjoleAjkular/AjkularApp/src/app/comment/comment.model.ts
+++ DjoleAjkular/AjkularApp/src/app/comment/comment.model.ts
@@ -1,6 +1,8 @@
+import { Accommodation } from "../accommodation/accommodation.model";
+
 export class Comment {
 
-    constructor(public Grade: number, public Text:string)
+    constructor(public Grade: number, public Text:string, public Accommodation: Accommodation)
     {
     }
 
--- DjoleAjkular/AjkularApp/src/app/place/place.model.ts
+++ DjoleAjkular/AjkularApp/src/app/place/place.model.ts
@@ -1,8 +1,9 @@
 import { Region } from "../region/region.model";
+import { Accommodation } from "../accommodation/accommodation.model";
 
 export class Place {
 
-    constructor(public Id: number, public Name:string, public Region: Region)
+    constructor(public Id: number, public Name:string, public Region: Region, public Accommodations: Accommodation[])
     {
     }
 
