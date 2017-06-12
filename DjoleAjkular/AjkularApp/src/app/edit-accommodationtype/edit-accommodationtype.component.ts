30f7995a730587aedf6bb9d262a5c13f0758f6af
borka94 <borka.pzs@gmail.com>
2017-06-12 11:02:41 +0200

    edit components created

---
+++ DjoleAjkular/AjkularApp/src/app/edit-accommodationtype/edit-accommodationtype.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-accommodationtype',
+  templateUrl: './edit-accommodationtype.component.html',
+  styleUrls: ['./edit-accommodationtype.component.css']
+})
+export class EditAccommodationtypeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ DjoleAjkular/AjkularApp/src/app/edit-roomreservation/edit-roomreservation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-roomreservation',
+  templateUrl: './edit-roomreservation.component.html',
+  styleUrls: ['./edit-roomreservation.component.css']
+})
+export class EditRoomreservationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
