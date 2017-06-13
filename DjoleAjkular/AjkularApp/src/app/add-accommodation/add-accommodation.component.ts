7744c7fbdfe959653558d5af4df7bf368e54ff74
borka94 <borka.pzs@gmail.com>
2017-06-13 10:04:18 +0200

    Accommodation updated

--- DjoleAjkular/AjkularApp/src/app/add-accommodation/add-accommodation.component.ts
+++ DjoleAjkular/AjkularApp/src/app/add-accommodation/add-accommodation.component.ts
@@ -4,33 +4,36 @@ import { AccommodationType } from "../accommodationtype/accommodationtype.model"
 import { Place } from "../place/place.model";
 import { Accommodation } from "../accommodation/accommodation.model";
 import { FormGroup } from "@angular/forms/forms";
+import { AccommodationTypeListService } from "../accommodationtype-list/accommodationtype-list.service";
 
 @Component({
   selector: 'add-accommodation',
   templateUrl: './add-accommodation.component.html',
   styleUrls: ['./add-accommodation.component.css'],
-  providers: [AccommodationListService]
+  providers: [AccommodationListService, AccommodationTypeListService]
 })
 export class AddAccommodationComponent implements OnInit {
 
-  @Input() accommodationtype: AccommodationType
+  @Input() accommodationtypes: AccommodationType[]
   @Input() place: Place
   @Output() onAccommodationAdded: EventEmitter<Accommodation>
   
-  constructor(private accommodationService: AccommodationListService)
+  constructor(private accommodationService: AccommodationListService, private accommodationtypeService: AccommodationTypeListService)
   {
     this.onAccommodationAdded = new EventEmitter();
   }
 
-  ngOnInit() { }
+  ngOnInit()
+  {
+    this.accommodationtypeService.getAll().subscribe(x => this.accommodationtypes = x.json() as AccommodationType[]);
+  }
 
   onSubmit(a: Accommodation, form: FormGroup)
   {
     form.reset();
     a.Approved=false;
+    a.AverageGrade=0;
     a.PlaceId=this.place.Id;
-    //a.AccommodationTypeId=this.accommodationtype.Id;
-    a.AccommodationTypeId=1;
     a.AppUserId=1;
     this.accommodationService.create(a).subscribe(x => this.place.Accommodations.push(x as Accommodation));
   }
