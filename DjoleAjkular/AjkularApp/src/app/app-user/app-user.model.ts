49d2a84a80325fe34e8726d5f62c24f175fa4129
borka94 <borka.pzs@gmail.com>
2017-06-15 13:43:10 +0200

    AppUser component added

---
+++ DjoleAjkular/AjkularApp/src/app/app-user/app-user.model.ts
@@ -0,0 +1,16 @@
+import { Accommodation } from "../accommodation/accommodation.model";
+import { Comment } from "../comment/comment.model";
+import { RoomReservation } from "../roomreservation/roomreservation.model";
+
+export class AppUser
+{
+    constructor (public Id?: number, public FirstName?: string, public LastName?: string, public Banned?: boolean, public Accommodations?: Accommodation[], public Comments?: Comment[], public RoomReservations?: RoomReservation[])
+    {
+        this.Accommodations=[];
+        this.Comments=[];
+        this.RoomReservations=[];
+        this.Banned=false;
+        this.FirstName="";
+        this.LastName="";
+    }
+}
